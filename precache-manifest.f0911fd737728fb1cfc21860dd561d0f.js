864efdcf57872fcb2d6ac7db882bc0143d2519ad
stranzer9 <souravsuman911@outlook.com>
2020-07-29 23:41:10 +0530

    Updates

---
+++ precache-manifest.f0911fd737728fb1cfc21860dd561d0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0446a433d71e396d768f585ca5daeef5",
+    "url": "/monster-cards/index.html"
+  },
+  {
+    "revision": "76f50bbdff2a812fa192",
+    "url": "/monster-cards/static/css/main.b67fd13d.chunk.css"
+  },
+  {
+    "revision": "c2b71e5b71dd0d4e8b34",
+    "url": "/monster-cards/static/js/2.6cee7051.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-cards/static/js/2.6cee7051.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76f50bbdff2a812fa192",
+    "url": "/monster-cards/static/js/main.5feb5560.chunk.js"
+  },
+  {
+    "revision": "277a004d42c0e06be2f8",
+    "url": "/monster-cards/static/js/runtime-main.c10e688f.js"
+  }
+]);
